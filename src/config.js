d6f78101779c78b7d1a6565b3f5b0c2aa928f51f
Priyansh Verma <20ce02009@iitbbs.ac.in>
2025-07-23 14:23:05 +0530

    udpates done

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
-// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||"https://cml-lab-server.vercel.app"|| 'http://localhost:5000';
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||"https://cml-lab-server.vercel.app"|| 'http://localhost:5000';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 // export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||"https://cml-lab-server-git-dev-priyanshhvs-projects.vercel.app"|| 'http://localhost:5000';
 // export const API_BASE_URL = "https://cml-lab-server-git-dev-priyanshhvs-projects.vercel.app"|| 'http://localhost:5000';
 
